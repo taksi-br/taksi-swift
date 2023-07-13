41be72dbe575c0c1f287173cb665b9e49f34b3a1
Mateus Lino <mateusantos05@gmail.com>
2023-07-13 16:16:58 -0300

    Add skeleton text

--- Sources/UI/ComponentViews/CollectionComponentView.swift
+++ Sources/UI/ComponentViews/CollectionComponentView.swift
@@ -3,30 +3,22 @@
 import SwiftUI
 
 public struct CollectionComponentView: ComponentView, View {
-    final class ViewModel: ObservableObject {
-        @Published var component: CollectionComponent
-
-        init(component: CollectionComponent) {
-            self.component = component
-        }
-    }
-
-    @StateObject var viewModel: ViewModel
+    var component: CollectionComponent
     @Binding var renderMode: ComponentViewRenderMode
 
     public var body: some View {
         VStack {
-            Text("Collection of items")
-
             switch renderMode {
             case .skeleton:
-                Text("Collection of items")
+                ForEach(0..<3) { _ in
+                    Text(String.skeleton())
+                        .redacted(reason: .placeholder)
+                }
             case .content:
-                ForEach(viewModel.component.content.dynamicData.values, id: \.identifier) { item in
+                ForEach(component.content.dynamicData.values, id: \.identifier) { item in
                     item.view(renderMode: $renderMode)?.asSwiftUIView()
                 }
             }
-
         }
     }
 }
