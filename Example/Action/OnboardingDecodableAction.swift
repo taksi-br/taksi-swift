aea1ef00346e55ebad134519c9b5753a785f91f2
Mateus Lino <mateusantos05@gmail.com>
2023-07-19 16:11:12 -0300

    Delete example project to move it to dedicated repo

--- Example/Action/OnboardingDecodableAction.swift
+++
@@ -1,8 +0,0 @@
-// Created by Mateus Lino
-
-import Foundation
-import Taksi
-
-protocol OnboardingDecodableAction: Action {
-    init(from decoder: Decoder, dependencies: OnboardingDependencies) throws
-}
